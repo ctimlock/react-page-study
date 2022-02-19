f9d4a2856c16f6d61aa3d8aae93a1d3d2528719c
Charlie <ctimlock@gmail.com>
2022-02-19 15:41:14 +1100

    Added floating nav-bar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css'
 const App = () => {
   return (
     <div className='App'>
+      <Navbar />
       <div className='gradient__bg'>
-        <Navbar />
         <Header />
       </div>
       <Brand />
